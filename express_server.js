12128e8998f00e07d768d5b3e0d50bfe256cda0d
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-06-28 16:33:44 -0400

    created registration page and endpoint

--- express_server.js
+++ express_server.js
@@ -52,6 +52,13 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+
+app.get("/urls/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  
+  res.render("urls_register", templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
@@ -63,6 +70,16 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
+
+
+
 app.post("/urls", (req, res) => {
   
   const shortURL = generateRandomString();
@@ -70,11 +87,6 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
