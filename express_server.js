c8d2ff79e7f6be2c2b0da81632a13837258ec973
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-06-30 19:01:58 -0400

    updated login/logout handlers

--- express_server.js
+++ express_server.js
@@ -8,24 +8,14 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const generateRandomString = () => {
-  let characters = 'abcdefghijklmnopqrstuvwxyz';
-  let string = "";
-  
-  for (let i = 0; i < characters.length; i++) {
-    string += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return string.slice(0, 8);
-};
 
 
-    
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-  
-//\/\/\/THIS IS ONLY STARTER CODE\/\/\/////////
+
+
 const users = {
   "userRandomID":
   {id: "userRandomID",
@@ -36,30 +26,47 @@ const users = {
     email: "user2@example.com",
     password: "dishwasher-funk"}
 };
-//^^^THIS IS ONLY STARTER CODE^^^////////
+
+///////// HELPER FUNCTIONS///////////////////////////
+const emailLookup = (object, email) => {
+  for (const id in object) {
+    if (object[id].email === email) {
+      return id;
+    }
+  }
+};
+
+const generateRandomString = () => {
+  let characters = 'abcdefghijklmnopqrstuvwxyz';
+  let string = "";
+  
+  for (let i = 0; i < characters.length; i++) {
+    string += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return string.slice(0, 8);
+};
+
+////////// HELPER FUNCTIONS/////////////////////////
+  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+///////////GETS/////////////////////////////////////
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
-
-
 app.get("/urls", (req, res) => {
   const userID = req.cookies.userID;
-  
-  // if (!userID) {
-  //   res.send('not logged in!');
-  //   return;
-  // }
   const user = users[userID];
+  
   console.log(req.cookies);
   const templateVars = { urls: urlDatabase, user };
   res.render("urls_index", templateVars);
 });
+  
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { user: users[req.cookies["userID"]] };
@@ -68,10 +75,16 @@ app.get("/urls/new", (req, res) => {
 
 
 app.get("/register", (req, res) => {
-  
   const templateVars = { user: null };
   
   res.render("register", templateVars);
+});
+  
+
+app.get("/login", (req, res) => {
+  const templateVars = { user: null };
+  
+  res.render("login", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -82,18 +95,17 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL,
     user: users[req.cookies["userID"]]
   };
+
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  
   const longURL = urlDatabase[req.params.shortURL];
+  
   res.redirect(longURL);
 });
 
-
-
-
+/////////////POSTS///////////////////////////////////
 
 app.post("/urls", (req, res) => {
   
@@ -116,37 +128,74 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("userID");
-  res.redirect("/urls");
-});
-
-app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  const id = emailLookup(users, email);
 
-  const id = generateRandomString();
-  
-  users[id] = { id, email, password };
-
-
-  if (!email || !password) {
-    res.status(400).send('Error code 400');
-    
+  if (!id) {
+    res.status(403).send('Account does not exist.');
+    return;
   }
 
-  if (users[email]) {
-    res.status(400).send('user already exists');
-    
+  if (users[id].password !== password) {
+    res.status(403).send("Invalid password.");
+    return;
   }
   
   res.cookie('userID', id);
   res.redirect('/urls');
- 
 });
+
+app.post("/logout", (req, res) => {
+
+  res.clearCookie("userID");
+  res.redirect("/urls");
+});
+  
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    res.status(400).send('Error code 400');
+    return;
+  }
+
+  if (emailLookup(users, email)) {
+    res.status(400).send('user already exists');
+    return;
+  }
+  
+  const id = generateRandomString();
+  users[id] = { id, email, password };
+  
+  res.cookie('userID', id);
+  res.redirect('/urls');
+});
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+
+  
+  
+
+
+
+ 
+
+
+
+  
   
 
   
